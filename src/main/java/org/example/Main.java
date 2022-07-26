9add807ec8538d9e33c3768ddc1fe42c52ab99a7
Nazar <litvik.12@gmail.com>
2022-07-26 18:28:40 +0300

    brach mate was deleted

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        char[] chars = {'H','e','l','l','o','M','a','t','e','s'};
-        for (int i = 0; i < chars.length; i++) {
-            System.out.println(chars[i]);
-        }
+        System.out.println("Hello mates!");
     }
 }
